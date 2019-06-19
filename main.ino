90c063cc65839d7c82b68355376af8507218316c
Mytowski <tomasz.mytowski@gmail.com>
2019-06-19 13:35:35 +0200

    Update main.ino
    Pin configuration

--- main.ino
+++ main.ino
@@ -1,3 +1,13 @@
+
+//CONFIG 
+
+const static int PIN_LIGHT_SENSOR = 18; // interrupt pin
+const static int PIN_SOUND_SENSOR = 19; // interrupt pin
+const static int PIN_BUTTON       = 20;
+
+
+//PROGRAM
+
 void setup() {
 
 }
