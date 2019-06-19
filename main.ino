1b60d3e055b1dd8ec71ac20fab933eff1e65e755
Mytowski <tomasz.mytowski@gmail.com>
2019-06-19 13:51:06 +0200

    LCD
    Add LCD and welcome screen

--- main.ino
+++ main.ino
@@ -1,3 +1,4 @@
+#include <LiquidCrystal.h>
 
 //CONFIG 
 
@@ -5,11 +6,18 @@ const static int PIN_LIGHT_SENSOR = 18; // interrupt pin
 const static int PIN_SOUND_SENSOR = 19; // interrupt pin
 const static int PIN_BUTTON       = 20;
 
+//
+
+LiquidCrystal lcd(2, 3, 4, 5, 6, 7); 
 
 //PROGRAM
 
 void setup() {
-
+  lcd.begin(16, 2);
+  lcd.setCursor(0, 0);
+  lcd.print("Thunderb. meter");
+  lcd.setCursor(0, 1);
+  lcd.print("Press button");
 }
 
 void loop() {
--- readme.md
+++ readme.md
@@ -7,3 +7,15 @@ Thunderbolt distance meter is a cheap device designed to measure how far thunder
 - 16x2 display (HD44780)
 - light sensor (poteneiometer+ photoresistor + LM393)
 - sound sensor (poteneiometer+ microphone + LM393)
+- button
+
+# Electrical connections:
+PIN 2 - LCD_RS
+PIN 3 - LCD ENABLE
+PIN 4 - LCD_D4
+PIN 5 - LCD_D5
+PIN 6 - LCD_D6
+PIN 7 - LCD_D7
+PIN 18 - Light sensor output
+PIN 19 - Sound sensor output
+PIN 20 - Button 
